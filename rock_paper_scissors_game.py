38687d31b4eb78e4137843b8894b017fdf9e34a0
MUHAMMED SHIYAD <71961452+SHIYADBAVA@users.noreply.github.com>
2023-10-06 00:27:44 +0530

    Create rock_paper_scissors_game.py
    Here's a simple implementation of the Rock, Paper, Scissors game in Python. In this game, the user chooses either rock, paper, or scissors, and the computer also makes a random choice. The winner is determined based on the rules: rock beats scissors, scissors beats paper, and paper beats rock.

---
+++ rock_paper_scissors_game.py
@@ -0,0 +1,40 @@
+import random
+
+def get_user_choice():
+    user_choice = input("Enter Rock, Paper, or Scissors: ").lower()
+    while user_choice not in ["rock", "paper", "scissors"]:
+        print("Invalid choice. Please try again.")
+        user_choice = input("Enter Rock, Paper, or Scissors: ").lower()
+    return user_choice
+
+def get_computer_choice():
+    choices = ["rock", "paper", "scissors"]
+    return random.choice(choices)
+
+def determine_winner(user_choice, computer_choice):
+    if user_choice == computer_choice:
+        return "It's a tie!"
+    elif (user_choice == "rock" and computer_choice == "scissors") or \
+         (user_choice == "scissors" and computer_choice == "paper") or \
+         (user_choice == "paper" and computer_choice == "rock"):
+        return "You win!"
+    else:
+        return "Computer wins!"
+
+def play_game():
+    print("Welcome to Rock, Paper, Scissors!")
+    while True:
+        user_choice = get_user_choice()
+        computer_choice = get_computer_choice()
+        print(f"You chose: {user_choice}")
+        print(f"Computer chose: {computer_choice}")
+        print(determine_winner(user_choice, computer_choice))
+        
+        play_again = input("Do you want to play again? (yes/no): ").lower()
+        if play_again != "yes":
+            print("Thanks for playing! Goodbye!")
+            break
+
+# Main function to start the game
+if __name__ == "__main__":
+    play_game()
