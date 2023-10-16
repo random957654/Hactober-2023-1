16896271142bb122314578e0b645f564b4534557
Rahul Jangra <106389897+leonado10000@users.noreply.github.com>
2023-10-16 18:20:09 +0530

    added 4. Median of Two Sorted Arrays

--- BS.cpp
+++ BS.cpp
@@ -1,36 +1,108 @@
+// #include <bits/stdc++.h>
+// using namespace std;
+// int* bubbleSort(int arr[], int n) {
+
+  
+//   for (int i = 0; i< n; i++) {
+      
+    
+//     for (int j = 0; j < n -1- i; j++) {
+
+    
+//       if (arr[j] > arr[j + 1]) {
+//         int temp = arr[j];
+//         arr[j] = arr[j+ 1];
+//         arr[j + 1] = temp;
+//       }
+//     }
+//   }
+//   return arr;
+// }
+
+// int binarySearch(int arr[], int x, int low, int high) {
+  
+	
+//   while (low <= high) {
+//     int mid = low + high / 2;
+
+//     if (arr[mid] == x)
+//       return mid;
+
+//     if (arr[mid] < x)
+//       low = mid + 1;
+
+//     else
+//       high = mid - 1;
+//   }
+
+//   return -1;
+// }
+
+// int main() {
+//   int n,x;
+// 	cout<<"Enter the size of the array"<<endl;
+// 	cin>>n;
+// 	int arr[n];
+//     cout<<"Enter the elements of the array"<<endl;    
+// 	for(int i=0;i<n;i++)
+// 	cin>>arr[i];
+// 	cout<<"Enter the element you want to search in the array"<<endl;
+// 	cin>>x;
+//     bubbleSort(arr,n);
+//   int result = binarySearch(arr, x, 0, n-1 );
+//   if (result == -1)
+//     printf("Not found");
+//   else
+//     printf("Element is found at index %d", result);
+// }
 #include <bits/stdc++.h>
 using namespace std;
+void bubbleSort(int arr[], int n) {
 
-int binarySearch(vector<int> &arr, int target)
-{
-    int low = 0;
-    int high = arr.size() - 1;
-    while (low <= high)
-    {
-        int mid = low + (high - low) / 2;
-        // target found.
-        if (arr[mid] == target)
-            return mid;
-        // If target is smaller, ignore right half
-        if (arr[mid] > target)
-            high = mid - 1;
-        // If target is greater, ignore left half
-        else
-            low = mid + 1;
+  
+  for (int i = 0; i< n; i++) {
+    for (int j = 0; j < n -1- i; j++) {
+      if (arr[j] > arr[j + 1]) {
+        int temp = arr[j];
+        arr[j] = arr[j+ 1];
+        arr[j + 1] = temp;
+      }
     }
-    // element is not present
-    return -1;
+  }
 }
 
-int main()
-{
-    vector<int> arr = {2, 3, 4, 10, 40};
-    int target = 1;
-    int idx = binarySearch(arr, target);
+int binarySearch(int arr[], int x, int low, int high) {
+  while (low <= high) {
+    int mid = low + high / 2;
 
-    if (idx == -1)
-        cout << "Element is not present in array";
+    if (arr[mid] == x)
+      return mid;
+
+    if (arr[mid] < x)
+      low = mid + 1;
+
     else
-        cout << "Element is present at index: " << result;
-    return 0;
+      high = mid - 1;
+  }
+
+  return -1;
+}
+
+int main() {
+  int n,x;
+    cout<<"Enter the size of the array"<<endl;
+    cin>>n;
+    int arr[n];
+    cout<<"Enter the elements of the array"<<endl;    
+    for(int i=0;i<n;i++)
+    cin>>arr[i];
+    cout<<"Enter the element you want to search in the array"<<endl;
+    cin>>x;
+
+    bubbleSort(arr,n);
+  int result = binarySearch(arr, x, 0, n-1 );
+  if (result == -1)
+    printf("Element is not present in the array");
+  else
+    printf("Element is found at position %d", result +1);
 }
