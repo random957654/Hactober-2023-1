fb8901e94a10823addc84ed72fbf3eaddfbca29f
Ambady <119492907+Pride04@users.noreply.github.com>
2023-10-18 10:18:58 +0530

    Create login.jsx

---
+++ login.jsx
@@ -0,0 +1,38 @@
+import React, { useState } from 'react';
+
+const Login = () => {
+  const [username, setUsername] = useState('');
+  const [password, setPassword] = useState('');
+
+  const handleUsernameChange = (event) => {
+    setUsername(event.target.value);
+  };
+
+  const handlePasswordChange = (event) => {
+    setPassword(event.target.value);
+  };
+
+  const handleLogin = () => {
+    // Add your login logic here, e.g., API call to authenticate the user
+    console.log('Logging in with:', { username, password });
+  };
+
+  return (
+    <div>
+      <h2>Login</h2>
+      <label>
+        Username:
+        <input type="text" value={username} onChange={handleUsernameChange} />
+      </label>
+      <br />
+      <label>
+        Password:
+        <input type="password" value={password} onChange={handlePasswordChange} />
+      </label>
+      <br />
+      <button onClick={handleLogin}>Login</button>
+    </div>
+  );
+};
+
+export default Login;
