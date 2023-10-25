9384b0f8362fc2bfc44dbabe87cfa29ee1e8b984
princesulekhiya-code <146360988+princesulekhiya-code@users.noreply.github.com>
2023-10-25 20:42:17 +0530

    Update swap.cpp
    Support for input output

--- swap.cpp
+++ swap.cpp
@@ -1,17 +1,24 @@
 // C++ program for illustration of swap() function 
 #include <bits/stdc++.h> 
-using namespace std; 
 
-int main() 
-{ 
-	int a = 10; 
-	int b = 20; 
-	cout << "Value of a before: " << a << endl; 
-	cout << "Value of b before: " << b << endl; 
+#include <iostream>
+using namespace std;
 
-	// swap values of the variables 
-	swap(a, b); 
-	cout << "Value of a now: " << a << endl; 
-	cout << "Value of b now: " << b << endl; 
-	return 0; 
-} 
+int main() {
+    int a;
+    int b;
+
+    cout << "Enter value of a: ";
+    cin >> a;
+
+    cout << "Enter value of b: ";
+    cin >> b;
+
+    // Swap values of the variables
+    swap(a, b);
+
+    cout << "Value of a now: " << a << endl;
+    cout << "Value of b now: " << b << endl;
+
+    return 0;
+}
