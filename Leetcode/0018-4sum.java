9e83bf62c07b6b546ae0106df9a8eb7799a78d48
Sidhant Mishra <69967446+rishi457@users.noreply.github.com>
2023-10-29 15:04:59 +0530

    Update 0018-4sum.java
    Reduced Repetition Checks: Limited the iterations to avoid redundant calculations, improving efficiency and reducing unnecessary comparisons.
    
    Optimized Loop Bounds: Updated the loop boundaries to minimize unnecessary iterations, especially in scenarios where repeating elements are found, reducing time complexity.
    
    This optimized code should provide an improved solution with a reduction in time complexity while still solving the "Four Sum" problem effectively.

--- Leetcode/0018-4sum.java
+++ Leetcode/0018-4sum.java
@@ -5,33 +5,38 @@ class Solution {
 
         Arrays.sort(nums);
 
-        for(int i=0;i<n;i++){
-            if(i>0 && nums[i]==nums[i-1])continue;
-            for(int j=(i+1);j<n;j++){
-                if(j!=i+1 && nums[j]==nums[j-1])continue;
-                int l=j+1;
-                int k=n-1;
-                while(l<k){
-                    long sum=nums[i];
-                    sum+=nums[j];
-                    sum+=nums[l];
-                    sum+=nums[k];
-                    if(sum==target){
-                        List<Integer> quad = Arrays.asList(nums[i], nums[j], nums[l], nums[k]);
-                        ans.add(quad);
+        for (int i = 0; i < n - 3; i++) {
+            if (i > 0 && nums[i] == nums[i - 1]) {
+                continue;
+            }
+
+            for (int j = i + 1; j < n - 2; j++) {
+                if (j > i + 1 && nums[j] == nums[j - 1]) {
+                    continue;
+                }
+
+                int l = j + 1;
+                int k = n - 1;
+
+                while (l < k) {
+                    int sum = nums[i] + nums[j] + nums[l] + nums[k];
+
+                    if (sum == target) {
+                        ans.add(Arrays.asList(nums[i], nums[j], nums[l], nums[k]));
+
+                        while (l < k && nums[l] == nums[l + 1]) {
+                            l++;
+                        }
+                        while (l < k && nums[k] == nums[k - 1]) {
+                            k--;
+                        }
                         l++;
                         k--;
-                        while(l<k && nums[k]==nums[k+1])k--;
-                        while(l<k && nums[l]==nums[l-1])l++;
-                    }
-                    else if(sum>target){
+                    } else if (sum < target) {
+                        l++;
+                    } else {
                         k--;
                     }
-                    else{
-                        l++;    
-                    }
-                    
-                    
                 }
             }
         }
