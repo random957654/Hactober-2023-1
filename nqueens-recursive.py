6a29506ff8e5e917a656868b22ddeace7081e2c8
prashansa9583 <84851183+prashansa9583@users.noreply.github.com>
2023-10-30 11:52:16 +0530

    Create nqueens-recursive.py

---
+++ nqueens-recursive.py
@@ -0,0 +1,49 @@
+def print_board(board):
+    n = len(board)
+    for i in range(n):
+        for j in range(n):
+            print(board[i][j], end=" ")
+        print()
+
+def is_safe(board, row, col, n):
+    for i in range(col):
+        if board[row][i] == 1:
+            return False
+
+    for i, j in zip(range(row, -1, -1), range(col, -1, -1)):
+        if board[i][j] == 1:
+            return False
+
+    for i, j in zip(range(row, n, 1), range(col, -1, -1)):
+        if board[i][j] == 1:
+            return False
+
+    return True
+
+def solve_n_queens_util(board, col, n):
+    if col >= n:
+        return True
+
+    for i in range(n):
+        if is_safe(board, i, col, n):
+            board[i][col] = 1
+
+            if solve_n_queens_util(board, col + 1, n):
+                return True
+
+            board[i][col] = 0
+
+    return False
+
+def solve_n_queens(n):
+    board = [[0 for _ in range(n)] for _ in range(n)]
+
+    if not solve_n_queens_util(board, 0, n):
+        print("Solution does not exist")
+        return None
+
+    print_board(board)
+
+# Driver code
+n = 4
+solve_n_queens(n)
